e312c3592694fd9e1f8c30e75da9b380ad1af2dd
akam alam <arundelfootball23@gmail.com>
2020-09-23 05:20:47 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -23,12 +23,12 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-  divide = num1/num2 
+  divide= num1/num2 
   divide
 end
 
 def square_root(num)
-  root = Math.sqrt(num)
+  root= Math.sqrt(num)
   root 
 
 end
